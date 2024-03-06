c84d76daeeec65856ecbcb2070f36379ec7e9393
matias-rojas-nest <matias.rojas@nestconsultores.cl>
2024-03-06 14:53:17 -0300

    Login App  - UBB

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+dotenv.config()
+
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/login', (req, res) => {
+  const { usuario, clave } = req.body
+
+  // Verificar si el usuario y la clave son correctos
+  if (usuario === process.env.USER_UBB && clave === process.env.PASS_UBB) {
+    res.json({ success: true })
+  } else {
+    res
+      .status(401)
+      .json({ success: false, message: 'Usuario o clave incorrectos' })
+  }
+})
+
+// mostrar datos de formularios
+app.use(express.json({ limit: '2mb' }))
+app.use(cors())
+
+// port
+const port = 3003
+
+app.listen(port, () => {
+  console.log(`Server's running at port ${port}`)
+})
